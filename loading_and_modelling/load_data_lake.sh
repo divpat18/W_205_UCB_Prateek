ea539e4ba0478291cadd685c1d05d6e6b91dab0d
divpat18 <divyangprateek@gmail.com>
2016-02-20 16:48:09 +0000

    Removing old file location

--- loading_and_modelling/load_data_lake.sh
+++
@@ -1,5 +0,0 @@
-hdfs dfs -put /data/medical/modified/effective_care.csv /user/w205/hospital_compare/effective_care
-hdfs dfs -put /data/medical/modified/hospitals.csv /user/w205/hospital_compare/hospitals
-hdfs dfs -put /data/medical/modified/readmissions.csv /user/w205/hospital_compare/readmissions
-hdfs dfs -put /data/medical/modified/measures.csv /user/w205/hospital_compare/measures
-hdfs dfs -put /data/medical/modified/surveys_responses.csv /user/w205/hospital_compare/survey_responses
